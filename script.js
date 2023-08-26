4bb4da3a8674978ff1034df3add38c0ffa4aaf48
SempaiOchilov <139633008+SempaiOchilov@users.noreply.github.com>
2023-08-26 14:58:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let name ="Djon"
+let admin ="Djon"
+
+let balance= 25000
+
+let car = 4480
+let food= 890
+
+let invoice= 5500
+
+let stock= 4200
+let totalbalance = 0
+
+console.log(name 
+  + " " + "его баланс"+ " "+ balance + " "+ "он тратит на машину" + " " + car 
+  + " " + "на еду " + " " + food +" " + "его счет составляет" + " " + invoice + " и у него есть запас в размере " + stock  +" " + " в конце у него остается" 
+  + " " + totalbalance );
